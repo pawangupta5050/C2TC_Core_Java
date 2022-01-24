65da9112ee55d899b332b901641e855df20d1720
pawangupta5050 <85182279+pawangupta5050@users.noreply.github.com>
2022-01-24 11:29:19 +0530

    First Commit

---
+++ C2TC_M05/src/Core_Java/HashCode.java
@@ -0,0 +1,26 @@
+package Core_Java;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashCode {
+
+	public static void main(String[] args)
+	{
+	     SortedSet<String> hs=new SortedSet<String>();
+	hs.add("Hash");
+	hs.add("Set");
+	hs.add("Demo");
+	hs.add("Demo");
+	Iterator i=hs.iterator();
+	while(i.hasNext())
+	{
+	Object o=i.next();
+	System.out.println(0);
+	}
+	// TODO Auto-generated method stub
+
+	}
+
+	}
+
+}
